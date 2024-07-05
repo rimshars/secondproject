0aac26ecbc0c59c78fe5788498437b0924aef52c
rimshars <rs1820902@gmail.com>
2024-07-04 17:12:07 -0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex">
+      <div className="bg-black text-white w-[230px] h-screen">
+        <div>
+       <img className="w-[100px] h-[100px] rounded-full mx-auto mt-14 border-white border-2"
+       src="cat.jpg" alt=""/>
+        </div>
+        </div>
+
+
+  
+
+    </main>
+  );
+}
+      
+  
